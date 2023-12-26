3fd8f0a32db6176985fe657f0d7814d63288f650
Amine Benmoussa <121407324+Amine-Nova@users.noreply.github.com>
2023-12-26 18:03:35 +0100

    start CPP6

---
+++ CPP6/ex00/ScalarConverter.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenmous <abenmous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/25 16:25:31 by abenmous          #+#    #+#             */
+/*   Updated: 2023/12/25 16:25:34 by abenmous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{
+}
+
+// void ScalarConverter::convert(std::string input)
+// {
+//  static_cast<int>(input);
+//  char *str = NULL;
+//  strtod(input.c_str(), )
+// }
+
+ScalarConverter::~ScalarConverter()
+{
+}
+// input 
---
+++ CPP6/ex00/ScalarConverter.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abenmous <abenmous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/25 16:13:29 by abenmous          #+#    #+#             */
+/*   Updated: 2023/12/25 18:03:42 by abenmous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<iostream>
+
+class ScalarConverter
+{
+private:
+public:
+    static void convert(char str);
+    ScalarConverter();
+    ~ScalarConverter();
+};
